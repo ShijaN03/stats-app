ab9356afb162260a147f6b30cd30fefb9bde5809
ShijaN03 <shijan.ks@yandex.ru>
2025-12-08 22:36:29 +0300

    init commit

---
+++ StatisticsTestTask/UIComponents/UIConstants/UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor.swift
+//  StatisticsTestTask
+//
+//  Created by shijan on 08.12.2025.
+//
+
+import UIKit
+
+public extension UIColor {
+    
+    static var mainThemeBackground: UIColor {
+        UIColor(red: 246/256, green: 246/256, blue: 246/256, alpha: 1)
+    }
+}
---
+++ StatisticsTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StatisticsTestTask
+//
+//  Created by shijan on 08.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .mainThemeBackground
+    }
+
+
+}
+
